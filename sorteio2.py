2533ef6c1dfd193d8f67b80eac0a283bdfc74fbe
Kemuell Klinger <kemuellklinger411@gmail.com>
2024-01-02 19:57:37 -0300

    Scroll

--- sorteio2.py
+++ sorteio2.py
@@ -25,20 +25,33 @@ def pergarTipo():
         getTp = "?"
     return getTp
 
-def sorteio():
-
+def sorteio(event=None):  # Adicionamos um parâmetro de evento para o comando do botão e configuração do evento de rolagem do mouse
     addTodosNomes()
     tp = pergarTipo()
 
-    #----------RESET NO CAMPO "SORTEADOS"
     global result
     result.destroy()
     result = Frame(campo, width=250, height=380)
     result.pack()
-    #----------
+
+    canvas = Canvas(result, width=250, height=380)
+    canvas.pack(side="left", fill="both", expand=True)
+
+    scrollbar = Scrollbar(result, command=canvas.yview)
+    scrollbar.pack(side="right", fill="y")
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    
+    # Adicionamos os eventos de rolagem do mouse
+    canvas.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(int(-1 * (event.delta / 120)), "units"))
+    canvas.bind_all("<Button-4>", lambda event: canvas.yview_scroll(-1, "units"))
+    canvas.bind_all("<Button-5>", lambda event: canvas.yview_scroll(1, "units"))
+
+    frame = Frame(canvas)
+    canvas.create_window((0, 0), window=frame, anchor="nw")
 
     for i in range(len(nomes)):
-        
         num = random.randint(0, len(nomes) - 1)
         
         if len(nomes)>1 or num<len(nomes):
@@ -51,20 +64,20 @@ def sorteio():
                 if k % 2 == 0:
                     nomesSorteado.set(sorteados[k])
                     
-                    mostrar = Label(result, textvariable=nomesSorteado)
-                    mostrar.place(x=5, y=35 * k)
-                
-                    tipoSorteio = Label(result, text=tp, padx=20)
-                    tipoSorteio.place(x=90, y=(35 * k) + 20)
+                    mostrar = Label(frame, textvariable=nomesSorteado)
+                    mostrar.grid(row=k, column=0)
+                    
+                    tipoSorteio = Label(frame, text=tp, padx=20)
+                    tipoSorteio.grid(row=k, column=1)
                    
                 elif k % 2 == 1:
                     nomesSorteado.set(sorteados[k])
                     
-                    mostrar = Label(result, textvariable=nomesSorteado)
-                    mostrar.place(x=190, y=35 * k)
+                    mostrar = Label(frame, textvariable=nomesSorteado)
+                    mostrar.grid(row=k, column=2)
 
-                    barra = Label(result, text="-"*48)
-                    barra.place(x=0, y=35*k + 20)
+                    # barra = Label(frame, text="-" * 20)
+                    # barra.grid(row=k, column=3)
 
     sorteados.clear()
 
